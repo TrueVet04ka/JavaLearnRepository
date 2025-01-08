1361c2e0ce5707cbf74f382e055f664125f95969
Vet04ka <dimonlimon260501@gmail.com>
2025-01-08 12:24:19 +0200

    Calculator (Switch-case realisation)

--- JavaCore/Calculator.java
+++ JavaCore/Calculator.java
@@ -4,8 +4,82 @@ import java.util.Scanner;
 public class Calculator {
 
 public static void CalculatorMain(){
-String whiler = "Y";
+    String whiler = "Y";
+    do{
+    System.out.println("Это калькулятор");
+    
+Scanner scanner = new Scanner(System.in);
+System.out.print("Введите 1 число: ");
+int num1 = scanner.nextInt();
 
+scanner = new Scanner(System.in);
+System.out.print("Введите 2 число: ");
+int num2 = scanner.nextInt();
+
+int res;
+
+System.out.print("Действие: ");
+String Action = scanner.next();
+
+switch (Action) {
+    case "+":
+        res = num1 + num2;
+        System.out.println("Результат: " + res);
+        break;
+    case "-":
+        res = num1 - num2;
+        System.out.println("Результат: " + res);
+        break;
+    case "*":
+        res = num1 * num2;
+        System.out.println("Результат: " + res);
+        break;
+     case "/":
+        if (num2 == 0) {
+            System.out.println("Делитель не может равняться 0");
+        }
+        else
+        {
+        res = num1 / num2;
+        System.out.println("Результат: " + res);
+        }
+        break;
+    default:
+    System.out.println("Чёт не то");
+        break;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+String whiler = "Y";
 do{
 System.out.println("Это калькулятор");
 
@@ -47,7 +121,11 @@ whiler = scan.next();
 
     
 }while (whiler.equals("Y") || whiler.equals("y"));
-
+*/
+System.out.print("Продолжаем? (Y/N): ");
+scanner = new Scanner(System.in);
+whiler = scanner.next();
+}while (whiler.equals("Y") || whiler.equals("y"));
 }
 
 
