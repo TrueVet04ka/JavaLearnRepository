426ba93e398e247ec717f03ee4e3601fe5517afd
Vet04ka <dimonlimon260501@gmail.com>
2025-02-02 22:00:56 +0200

    Фан факт с инкрементом
    Запись
    array[3] = array[2] = array[1] = ++array[0];
    Делает вывод, в котором ячейки с 0 по 3 равняются 2.
    Запись
    array[3] = array[2] = array[1] = array[0]++;
    Вернёт со значением 2 только ячейку 0, ячейки с 1 по 3 будут со значением 1.
    Это связано с постфиксным и префиксным инкрементом
    Постфиксный инкремент возвращает и потом увеличивает значение.
    Префиксный сначала увеличивает значение, потом возвращает.

--- JavaCore/Main.java
+++ JavaCore/Main.java
@@ -4,12 +4,21 @@ public class Main {
     public static void main(String[] args)
     {
         //Car car = new Car(150, 2000, "White", new byte[]{0, 5, 4});
-
+        /* 
         Truck car2 = new Truck(150, 2000,  "White", new byte[]{0, 5, 4});
        car2.SetValues(500, 5000, "Blue", new byte[]{0, 6, 8}, true);
         car2.SetValues(0, 0, null, null);
-   
+*/
 
+
+int[] array = new int[10];
+
+array[0] = 1;
+
+array[3] = array[2] = array[1] = ++array[0];
+
+for (int i = 0; i < array.length; i++)
+System.out.println(array[i]);
    }
 }
 
