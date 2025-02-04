50ebce9709316a4ff1fb22919f22fc9af52812cf
Vet04ka <dimonlimon260501@gmail.com>
2025-02-05 00:06:48 +0200

    OOP 2

--- JavaCore/Main.java
+++ JavaCore/Main.java
@@ -4,12 +4,18 @@ public class Main {
  
     public static void main(String[] args)
     {
-        Car Mashinka = new Car();
-        Car Mashinka2 = new Car("Nyaaa", "pupu", 2004);
-        Mashinka.SetFields("Zaaza", "Caamel", 9998);
-        Mashinka.GetFields();
-        Mashinka2.GetFields();
+     Cat cat = new Cat();
+    cat.setName("Baarsik");
+     System.out.println( cat.GetName());
+     cat.makeSound();
+     Dog dog = new Dog();
+     dog.setName("sharik");
+     dog.setVaccinated(true);
+     dog.setBreed("Krutoy");
+    System.out.println(dog.GetName() + " " + dog.getVaccinated() + " " + dog.getBredd());
+   dog.makeSound();
     }
+
    
 }
 
