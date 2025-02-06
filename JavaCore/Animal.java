f1cdb9b56ff381517f0ae117721325b969dddfb8
Vet04ka <dimonlimon260501@gmail.com>
2025-02-06 18:26:36 +0200

    Интерфейс 2

--- JavaCore/Animal.java
+++ JavaCore/Animal.java
@@ -1,5 +1,8 @@
 public interface Animal {
 
     public void makeSound();
-    
+    public static void animalSound(Animal animal)
+    {
+        animal.makeSound();
+    };
 }
--- JavaCore/Main.java
+++ JavaCore/Main.java
@@ -6,14 +6,15 @@ public class Main {
         // Полиморфизм (Polymorphism)
         // Начальный уровень
         // Создайте интерфейс Animal с методом makeSound(). Реализуйте классы Dog и Cat, которые реализуют интерфейс.
-        Cat cat = new Cat();
-        Dog dog = new Dog();
-        cat.makeSound();
-        dog.makeSound();
+        Animal cat = new Cat();
+        Animal dog = new Dog();
+        Animal.animalSound(cat);
+        Animal.animalSound(dog);
 
         // Средний уровень
         // Напишите метод public static void animalSound(Animal animal), который принимает объект типа Animal и вызывает makeSound(). Передайте в него разные объекты.
         
+
         // Продвинутый уровень
         // Создайте метод, который принимает список List<Animal> и вызывает у каждого makeSound(). Используйте instanceof, чтобы выполнить дополнительное поведение для Dog и Cat.
     }
