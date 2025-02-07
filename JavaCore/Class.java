01b60b999031ab1259751202f32b1f65b8b266d0
Vet04ka <dimonlimon260501@gmail.com>
2025-02-07 17:11:44 +0200

    final2

--- JavaCore/Class.java
+++ JavaCore/Class.java
@@ -1,4 +1,7 @@
 public class Class {
     final String a = "Nya";
-       
+       public void func(final int x)
+       {
+        x += 10;
+       }
 }
--- JavaCore/Main.java
+++ JavaCore/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args)
     {
 Class s = new Class();
-s.a = "new";
+
         
     }
 
