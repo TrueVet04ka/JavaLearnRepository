6bd15a71b2ef399d859038e9050f34f5a52c82d4
Vet04ka <dimonlimon260501@gmail.com>
2025-02-07 17:10:36 +0200

    final 1

---
+++ JavaCore/Class.java
@@ -0,0 +1,4 @@
+public class Class {
+    final String a = "Nya";
+       
+}
--- JavaCore/Main.java
+++ JavaCore/Main.java
@@ -1,31 +1,10 @@
-import java.util.ArrayList;
 
 public class Main {
  
     public static void main(String[] args)
     {
-        // Базовый уровень
-        // Создание списка и добавление элементов
-        ArrayList <Integer> list = new  ArrayList<>(10);
-        for( int x = 0; x<10; x++)
-{
-            list.add(x);  
-}
-for( int x = 0; x<10; x++)
-{
-    System.out.println( list.get(x));
-             
-}
-        // Создайте ArrayList<Integer>, добавьте в него числа от 1 до 10 и выведите их на экран.
-        // Удаление элементов из списка
-        
-        // Удалите из списка все четные числа и выведите обновленный список.
-        // Поиск элемента в списке
-        
-        // Проверьте, содержит ли список заданное число, используя метод contains().
-        // Перебор списка разными способами
-        
-        // Используйте for, for-each и Iterator для вывода элементов списка.
+Class s = new Class();
+s.a = "new";
         
     }
 
